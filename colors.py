781ed94810fdbf19e08b14d30aff5e8ff234d9c9
Taylor <taylor.647@gmail.com>
2022-02-08 21:14:31 -0800

    Define console colors for the cube

---
+++ colors.py
@@ -0,0 +1,18 @@
+import colored
+
+white = colored.fg(15)
+blue = colored.fg(4)
+cyan = colored.fg(6)
+orange = colored.fg(172)
+yellow = colored.fg(184)
+red = colored.fg(1)
+green = colored.fg(2)
+
+facet_color = {
+    "U": white,
+    "B": cyan,
+    "L": orange,
+    "F": green,
+    "R": red,
+    "D": yellow
+}
